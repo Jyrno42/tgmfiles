0aea5e074feefb7d3da12c69b6663ae4731306e5
Jürno Ader <jyrno42@gmail.com>
2014-05-30 16:59:06 +0300

    py2 compability

--- tgmfiles/forms.py
+++ tgmfiles/forms.py
@@ -2,7 +2,9 @@ import os
 
 from django import forms
 from django.template.defaultfilters import filesizeformat
+from django.utils.translation import ugettext_lazy as _
 
+from tgmfiles.fields import TgmFormFileField
 from tgmfiles.models import TemporaryFileWrapper, get_max_file_size, human_readable_types, get_size_error
 
 
@@ -22,20 +24,17 @@ class TemporaryFileForm(forms.ModelForm):
     def __init__(self, real_field, *args, **kwargs):
         self.allowed_types = real_field.allowed_types
 
-        super().__init__(*args, **kwargs)
+        super(TemporaryFileForm, self).__init__(*args, **kwargs)
 
     def clean_file(self):
         uploaded_file = self.cleaned_data.get('file', False)
         if uploaded_file:
             if uploaded_file._size > get_max_file_size():
-                # TODO: Correct size for error.
                 raise forms.ValidationError(get_size_error())
         else:
-            raise forms.ValidationError("Couldn't read uploaded file")
+            raise forms.ValidationError(_("Couldn't read uploaded file"))
 
         if not allowed_type(uploaded_file.content_type, self.allowed_types):
-            # TODO: i18n and handle plurar form.
-            raise forms.ValidationError("File should be one of the following types [%s]" % (
-                human_readable_types(self.allowed_types)))
+            TgmFormFileField.file_type_error(uploaded_file.content_type, self.allowed_types)
 
         return uploaded_file
